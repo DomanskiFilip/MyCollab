ccd66600fa61e19ba796e31b48ba72983f73cfe3
DomanskiFilip <filipdomanski@outlook.com>
2024-01-16 13:40:02 +0000

    merge

--- Mycollabs/urls.py
+++ Mycollabs/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+# your url patterns
+
 
 urlpatterns = [
     path("", include("Core.urls")),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('collabs/', include('collabs.urls')),
 ]
 
-
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
--- collabs/views.py
+++ collabs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from collabs.models import Collab
 
 # Create your views here.
+
+def collab(request, pk):
+    collab = Collab.objects.get(pk=pk)
+
+    
+    return render(request, 'collabs/collab.html', {
+        'collab' : collab,
+    })
