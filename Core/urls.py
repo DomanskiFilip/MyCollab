1ec59f34f8a8c0b325de688292836c52b6b709e2
Dominik Wujek <dom.wujek@gmail.com>
2024-01-17 10:17:48 +0000

    further backend development

--- Core/urls.py
+++ Core/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from Core import views
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
+
+
+
 
 app_name = 'Core'
 
-
-
 urlpatterns = [
     path("", views.home, name="index"),
+    # path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    path('account/', views.account, name='account'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
