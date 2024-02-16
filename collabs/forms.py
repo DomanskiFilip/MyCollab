d5e7cbb8b38ba044fe4407c2e1c7b3977d7ccb7d
DomanskiFilip <filipdomanski@outlook.com>
2024-02-16 20:18:37 +0000

    changeing collabs format and adding new text field

--- collabs/forms.py
+++ collabs/forms.py
@@ -4,11 +4,11 @@ from .models import Collab, CollabImage
 class CollabForm(forms.ModelForm):
     class Meta:
         model = Collab
-        fields = ('title', 'description', 'tags')
+        fields = ('title', 'tags', 'description')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'input_title'}),
-            'description': forms.Textarea(attrs={'class': 'textarea_description'}),
             'tags': forms.CheckboxSelectMultiple(attrs={'class': 'checkbox_tags'}),
+            'description': forms.Textarea(attrs={'class': 'textarea_description'}),
         }
 
 # https://www.letscodemore.com/blog/django-inline-formset-factory-with-examples/
@@ -16,10 +16,11 @@ class CollabForm(forms.ModelForm):
 class CollabImageForm(forms.ModelForm):
     class Meta:
         model = CollabImage
-        fields = ('image', 'is_main')
+        fields = ('image', 'is_main', 'description_img')
         widgets = {
             'image': forms.ClearableFileInput(attrs={'class': 'input_image'}),
             'is_main': forms.CheckboxInput(attrs={'class': 'checkbox_is_main'}),
+            'description_img': forms.Textarea(attrs={'class': 'textarea_description'}),
         }
 
-CollabImageFormSet = forms.inlineformset_factory(Collab, CollabImage, form=CollabImageForm, extra=5, max_num=5, can_delete=True, can_delete_extra=False)
+CollabImageFormSet = forms.inlineformset_factory(Collab, CollabImage, form=CollabImageForm, extra=3, max_num=3, can_delete=True, can_delete_extra=False)
