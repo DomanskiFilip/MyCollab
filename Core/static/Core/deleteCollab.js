2d753e51016e32618e940298265005a711f70240
CasualStrategy <filip_domanski@outlook.com>
2024-03-13 15:11:04 +0000

    button optimalisation in collab view

--- Core/static/Core/deleteCollab.js
+++ Core/static/Core/deleteCollab.js
@@ -5,10 +5,16 @@ function confirmDelete(collabId) {
     let deleteBtn = document.getElementById("deleteBtn");
     deleteBtn.style.opacity = "0";
     
+    let editBtn = document.getElementById("editBtn");
+    editBtn.style.borderTopRightRadius = "10px";
+    editBtn.style.borderBottomRightRadius = "10px";
+
     let cancelDeleteBtn = document.getElementById("cancelDeleteBtn");
     cancelDeleteBtn.addEventListener("click", function() {
         deleteCollabModal.style.display = "none";
         deleteBtn.style.opacity = "1";
+        editBtn.style.borderTopRightRadius = "initial";
+        editBtn.style.borderBottomRightRadius = "initial";
     });
 
     window.onclick = function(event) {
